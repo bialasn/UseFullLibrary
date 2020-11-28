3e5e5192d86b0edadab2171982c75fef5706170d
bialasn <61627931+bialasn@users.noreply.github.com>
2020-11-28 12:32:48 +0100

    Init

---
+++ UsefullExtensionLib/app/src/main/java/com/nbprog/usefullextensionlib/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nbprog.usefullextensionlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nbprog.usefullextensionlibrary.px
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
