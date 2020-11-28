92be37027704654b6f977d742f07a4bb06aac237
bialasn <61627931+bialasn@users.noreply.github.com>
2020-11-28 15:23:40 +0100

    Added some ext

--- UsefullExtensionLib/app/src/main/java/com/nbprog/usefullextensionlib/MainActivity.kt
+++ UsefullExtensionLib/app/src/main/java/com/nbprog/usefullextensionlib/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.nbprog.usefullextensionlib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.nbprog.usefullextensionlibrary.dp
+import com.nbprog.usefullextensionlibrary.hideKeyboard
 import com.nbprog.usefullextensionlibrary.px
+import com.nbprog.usefullextensionlibrary.showKeyboard
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setShowHideOnClickListener()
+        convertTestLog()
+
+    }
+
+
+    private fun setShowHideOnClickListener() {
+        showKeyboard.setOnClickListener {
+            it.showKeyboard()
+        }
+        hideKeyboard.setOnClickListener {
+            it.hideKeyboard()
+        }
+    }
+
+    private fun convertTestLog() {
+        Log.i("convert Int to dp", 10.dp.toString())
+        Log.i("convert Int to px", 10.px.toString())
     }
 }
--- UsefullExtensionLib/usefullExtensionLibrary/src/main/java/com/nbprog/usefullextensionlibrary/IntExtension.kt
+++ UsefullExtensionLib/usefullExtensionLibrary/src/main/java/com/nbprog/usefullextensionlibrary/IntExtension.kt
@@ -2,9 +2,14 @@ package com.nbprog.usefullextensionlibrary
 
 import android.content.res.Resources
 
+/**
+ * Convert Int to dp
+ */
 val Int.dp: Int
     get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 
-
+/**
+ * Convert Int to px
+ */
 val Int.px: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
